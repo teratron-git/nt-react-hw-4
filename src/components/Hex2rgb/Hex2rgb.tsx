310e67fda682641e44a810cb6f618e1748d58ac4
Sergei Tereshkov <s.tereshkov@alarmtrade.ru>
2022-01-09 10:03:35 +0300

    fix: Hex2rgb needs to use state via useErrect

--- src/components/Hex2rgb/Hex2rgb.tsx
+++ src/components/Hex2rgb/Hex2rgb.tsx
@@ -1,9 +1,22 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import st from "./Hex2rgb.module.css"
 
 const Hex2rgb = () => {
   const [hex, setHex] = useState("")
   const [result, setResult] = useState("")
+
+  const hex2rgbConverter = (hexVal: string): number => parseInt(hexVal, 16)
+
+  useEffect(() => {
+    if (hex.length === 7) {
+      const r = `${hex[1]}${hex[2]}`
+      const g = `${hex[3]}${hex[4]}`
+      const b = `${hex[5]}${hex[6]}`
+      setResult(
+        hex.match(/^#[0-9a-f]{6}$/i) ? `rgb(${hex2rgbConverter(r)},${hex2rgbConverter(g)},${hex2rgbConverter(b)})` : "ОШИБКА!"
+      )
+    }
+  }, [hex])
 
   const hexHandler = (e: any) => {
     if (e.target.value.length !== 7) {
@@ -11,18 +24,8 @@ const Hex2rgb = () => {
       setResult("")
     } else {
       setHex(e.target.value)
-      const r = `${e.target.value[1]}${e.target.value[2]}`
-      const g = `${e.target.value[3]}${e.target.value[4]}`
-      const b = `${e.target.value[5]}${e.target.value[6]}`
-      setResult(
-        e.target.value.match(/^#[0-9a-f]{6}$/i)
-          ? `rgb(${hex2rgbConverter(r)},${hex2rgbConverter(g)},${hex2rgbConverter(b)})`
-          : "ОШИБКА!"
-      )
     }
   }
-
-  const hex2rgbConverter = (hexVal: string): number => parseInt(hexVal, 16)
 
   return (
     <div className={st.container} style={hex.length === 7 ? { backgroundColor: hex } : {}}>

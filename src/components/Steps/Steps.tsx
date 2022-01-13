ee5d81a6bda59d7a2822a04197506677754dd221
Sergei Tereshkov <s.tereshkov@alarmtrade.ru>
2022-01-13 17:24:57 +0300

    add: all functions for task 2

--- src/components/Steps/Steps.tsx
+++ src/components/Steps/Steps.tsx
@@ -1,23 +1,27 @@
-import { useEffect, useState } from "react"
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react"
 import DatePicker from "react-datepicker"
 import "react-datepicker/dist/react-datepicker.min.css"
 import { v4 as uuidv4 } from "uuid"
 import st from "./Steps.module.css"
 
+interface IResultItem {
+  id: string
+  date: Date
+  distance: number | string
+}
+
 const Steps = () => {
-  const [selectedDate, setSelectedDate] = useState(new Date())
-  const [selectedDistance, setSelectedDistance] = useState(1)
-  const [resultAmout, setResultAmount] = useState([])
+  const [selectedDate, setSelectedDate]: [Date, Dispatch<SetStateAction<Date>>] = useState<Date>(undefined)
+  console.log("🚀 ~ file: Steps.tsx ~ line 15 ~ Steps ~ selectedDate", selectedDate)
+  const [selectedDistance, setSelectedDistance]: [number | string, Dispatch<SetStateAction<number | string>>] = useState<
+    number | string
+  >(undefined)
+  const [resultAmout, setResultAmount] = useState<IResultItem[]>([])
+  const clickedDate = useRef(null)
+  const [clickedDate1, setClickedDate1] = useState()
+  console.log("🚀 ~ file: Steps.tsx ~ line 22 ~ Steps ~ clickedDate1", clickedDate1)
 
   console.log("🚀 ~ file: Steps.tsx ~ line 9 ~ Steps ~ resultAmout", resultAmout)
-
-  useEffect(() => {
-    const dateMs: any = Date.parse(selectedDate.toString())
-    const test = resultAmout.findIndex((item) => {
-      return item.date === dateMs
-    })
-    console.log("🚀 ~ file: Steps.tsx ~ line 32 ~ clickHandler ~ test", test)
-  }, [resultAmout])
 
   function formatDate(date: any) {
     const newDate = new Date(date)
@@ -27,8 +31,7 @@ const Steps = () => {
     let mm: number | string = newDate.getMonth() + 1
     if (mm < 10) mm = `0${mm}`
 
-    let yy: number | string = newDate.getFullYear() % 100
-    if (yy < 10) yy = `0${yy}`
+    const yy: number | string = newDate.getFullYear()
 
     return `${dd}.${mm}.${yy}`
   }
@@ -36,19 +39,48 @@ const Steps = () => {
   const submitHandler = (e: any) => {
     e.preventDefault()
     const dateMs: any = Date.parse(selectedDate.toString())
+    // console.log("🚀 ~ file: Steps.tsx ~ line 39 ~ submitHandler ~ dateMs", dateMs)
+    const found = resultAmout.findIndex((item) => item.date === dateMs)
+    // console.log("🚀 ~ file: Steps.tsx ~ line 41 ~ submitHandler ~ found", found)
+    if (found == -1) {
+      setResultAmount(
+        [...resultAmout, { id: uuidv4(), date: dateMs, distance: selectedDistance }].sort((a, b) => b.date - a.date)
+      )
+    } else {
+      resultAmout[found].date = dateMs
+      resultAmout[found].distance = +resultAmout[found].distance + +selectedDistance
+      setResultAmount([...resultAmout])
+    }
 
-    // console.log(formatDate(selectedDate))
-
-    const arr = [...resultAmout, { id: uuidv4(), date: dateMs, distance: selectedDistance }]
-    const arr2 = arr.sort((a, b) => b.date - a.date)
-
-    console.log("🚀 ~ file: Steps.tsx ~ line 27 ~ clickHandler ~ arr", arr2)
-    setResultAmount(arr2)
     console.log("🚀 ~ file: Steps.tsx ~ line 39 ~ clickHandler ~ resultAmout", resultAmout)
+    setSelectedDate(undefined)
+    setSelectedDistance("")
   }
 
   const changeDistance = (e: any) => {
     setSelectedDistance(e.target.value)
+  }
+  const deleteHandler = (id: any) => {
+    // setClickedDate1(clickedDateqwe)
+    // const dateMs: any = Date.parse(e.target.parentElement.children[0].innerHTML.toString())
+
+    console.log(
+      "111",
+      resultAmout.filter((item) => item.id !== id)
+    )
+    setResultAmount(resultAmout.filter((item) => item.id !== id))
+  }
+  const editHandler = (id: string, date: Date, distance: any) => {
+    // setClickedDate1(clickedDateqwe)
+    // const dateMs: any = Date.parse(e.target.parentElement.children[0].innerHTML.toString())
+
+    console.log("222", date)
+    // setResultAmount(resultAmout.filter((item) => item.id !== id))
+    deleteHandler(id)
+
+    setSelectedDate(new Date(date))
+    console.log("🚀 ~ file: Steps.tsx ~ line 83 ~ editHandler ~ new Date(date)", new Date(date))
+    setSelectedDistance(distance)
   }
 
   return (
@@ -63,12 +95,24 @@ const Steps = () => {
               selected={selectedDate}
               onChange={(date: Date) => setSelectedDate(date)}
               dateFormat="dd.MM.yyyy"
+              required
+              autoComplete="off"
+              maxDate={new Date()}
             />
           </div>
 
           <div className="distanceItem">
             <label htmlFor="distance">км</label>
-            <input type="number" name="distance" id="distance" value={selectedDistance} onChange={changeDistance} required />
+            <input
+              type="number"
+              name="distance"
+              id="distance"
+              value={selectedDistance}
+              onChange={changeDistance}
+              required
+              autoComplete="off"
+              step={0.1}
+            />
           </div>
 
           <input type="submit" name="button" id="button" value="OK" />
@@ -76,12 +120,20 @@ const Steps = () => {
 
         <div className={st.result}>
           <ol>
+            <li className={st.resultHeader}>
+              <span>Дата</span>
+              <span>Дистанция</span>
+              <span>Действия</span>
+            </li>
             {console.log(`render ${JSON.stringify(resultAmout)} `)}
             {resultAmout.map((item, i) => (
               <div key={item.id} className={st.resultItem}>
                 <li>
-                  {formatDate(item.date)} {item.distance}
-                  {console.log(`render ${i + 1} - ${formatDate(item.date)} `)}
+                  <span ref={clickedDate}>{formatDate(item.date)}</span>
+                  <span>{item.distance}</span>
+                  {/* {console.log(`render ${i + 1} - ${formatDate(item.date)} `)} */}
+                  <i className="far fa-edit" onClick={() => editHandler(item.id, item.date, item.distance)} />
+                  <i className="far fa-window-close" onClick={(e) => deleteHandler(item.id)} />
                 </li>
               </div>
             ))}
